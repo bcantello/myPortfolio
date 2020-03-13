f12f6dee6e57d0ccb77d61c9b660682df01e70c5
Brandon Cantello <bcantello@gmail.com>
2020-03-12 19:34:36 -0700

    adds jquery for finding animation-element on scroll

--- js/app.js
+++ js/app.js
@@ -98,3 +98,29 @@ $(window).on('scroll', function () {
     })
 });
 
+//Scroll based dynamic transitions
+let $animation_elements = $('.animation-element');
+let $window = $(window);
+
+$window.on('scroll resize', checkIfElementInView);
+$window.trigger('scroll');
+
+function checkIfElementInView() {
+    let window_height = $window.height();
+    let window_top_position = $window.scrollTop();
+    let window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+        let $element = $(this);
+        let element_height = $element.outerHeight();
+        let element_top_position = $element.offset().top;
+        let element_bottom_position = (element_top_position + element_height);
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
+}
